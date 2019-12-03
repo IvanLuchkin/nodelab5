610995c1b03442932208d85d265c58d270efac6f
IvanLuchkin <id.luchkin@gmail.com>
2019-12-03 18:59:38 +0200

    fix

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const app = express()
 const path = require('path')
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'page.html'))
-  res.sendFile(path.join(__dirname, 'lab5.js'))
 })
 const port = process.env.PORT || 8800
 app.listen(port, console.log('server listening...'))
