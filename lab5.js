807f4acbb7592cd9b58b71907e2fea15805da129
IvanLuchkin <id.luchkin@gmail.com>
2019-12-03 17:49:54 +0200

    added multiple fixes for successful Heroku deployment

---
+++ lab5.js
@@ -0,0 +1,22 @@
+function total () {
+    document.getElementById('result').value = 'The total price of the courses: $' + (coursePrice('Java') + coursePrice('Cpp') + coursePrice('Cs') + coursePrice('Js') + coursePrice('HtmlCss'))
+}
+function coursePrice (id) {
+    const form = document.getElementById(id + 'Form')
+    const checkbox = document.getElementById(id)
+    let price = 0
+    if (checkbox.checked) {
+        price += Number(checkbox.value)
+        price += parseInt(form.language.value)
+        price += parseInt(form.length.value)
+        price += parseInt(form.repForm.value)
+    }
+    return price
+}
+function showOrHide (id) {
+    if (document.getElementById(id).checked) {
+        document.getElementById(id + 'Options').style.display = 'block'
+    } else {
+        document.getElementById(id + 'Options').style.display = 'none'
+    }
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+const express = require('express')
+const app = express()
+const path = require('path')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'page.html'))
+})
+const port = process.env.PORT || 8800
+app.listen(port, console.log('server listening...'))
